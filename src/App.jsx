503850a06ae2f2ab6d58259997fba02219831673
atbeesley <alexander.beesley@gmail.com>
2023-04-03 08:16:05 +0100

    add linting

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,12 @@ const welcome = {
   greeting: "hiya"
 }
 
-const getTitle = (title) => {
-  return title
-}
-
 const arr = [1, 2, true, "fish", NaN]
 
 function App() {
   return (
     <div className="App">
       <h1>{welcome.title}</h1>
-      <h1>{getTitle("hiiiiiii")}</h1>
-      <h2>{welcome.greeting}</h2>
-      {arr.map((x) => (
-        <h1 key={x}>{x}</h1>
-      ))}
       <label htmlFor="search">
         <p>search</p>
         <input id="search" type="text"/>
