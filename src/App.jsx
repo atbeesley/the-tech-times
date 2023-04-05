24ef29299cc293a6c3ce0de20ef1c34bfa4fb4cb
atbeesley <alexander.beesley@gmail.com>
2023-04-05 10:40:50 +0100

    List mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import './App.css'
 
+const list = [
+  {
+    title: "React",
+    url: 'https://reactjs.org',
+    author: "Fish Man",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: 'https://redux.js.org',
+    author: "Andrew Clark, Dan Abramov",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
+
 const welcome = {
   title: "react!",
   greeting: "hiya"
 }
-
-const arr = [1, 2, true, "fish", NaN]
 
 function App() {
   return (
@@ -15,6 +32,11 @@ function App() {
         <p>search</p>
         <input id="search" type="text"/>
       </label>
+      <ul>
+      {list.map(function (x){
+        return <li>{x.title}</li>;
+      })}
+      </ul>
     </div>
   )
 }
