2e09c4e07543fee0b2752da5a054ccb07f84e9b5
atbeesley <alexander.beesley@gmail.com>
2023-04-07 17:38:34 +0100

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,62 @@
 import './App.css'
 
-const welcome = {
-  title: "hacker stories!",
-  greeting: "hiya"
-}
+const App = () =>{
+  
+  const listData = [
+    {
+      title: "React",
+      url: 'https://reactjs.org',
+      author: "Fish Man",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: 'https://redux.js.org',
+      author: "Andrew Clark, Dan Abramov",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ]
 
-const listData = [
-  {
-    title: "React",
-    url: 'https://reactjs.org',
-    author: "Fish Man",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: 'https://redux.js.org',
-    author: "Andrew Clark, Dan Abramov",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-]
-
-const App = () => (
+ return (
     <div className="App">
       <Search />
-      <List />
+      <List list={listData} />
     </div>
   )
+ }
 
-const List = () => (
-    <ul>
-    {listData.map((x) => {
-      return (
+const List = (props) => (
+  <ul>
+    {props.listData.map((x) => (
       <li key={x.objectID}>
         <h2>{x.title}</h2>
         <ul>
           <li><span>{x.num_comments}</span></li>
           <li><span>{x.points}</span></li>
-          <li><span>To learn more about {x.title}, click <a href={x.url} key={x.objectID} target="_blank">here</a>.</span></li>
-        </ul>
+          <li><span>To learn more about {x.title}, click <a href={x.url} key={x.objectID}>here</a>.</span></li>
+          </ul>
       </li>
-      )
-    })}
+    ))}
     </ul>
-   )
+   );
 
-const Search = () => (
+const Search = () => {
+
+  const handleChange = (event) =>{
+    console.log(event);
+    console.log(event.target.value);
+  }
+
+  return (
     <div id="search-box">
-      <h1>{welcome.title}</h1>
-        <label htmlFor="search">search: </label>
-      <input id="search" type="text" />
+      <label htmlFor="search">search: </label>
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   )
+}
 
 export default App
