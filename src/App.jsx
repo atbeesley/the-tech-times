6e73c48cc87da4754f91f4a25ffbc1f3369b102c
atbeesley <alexander.beesley@gmail.com>
2023-04-17 17:07:30 +0100

    delete story btn working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
 import * as React from 'react';
 
+const initialStories = [
+  {
+    title: "React",
+    url: 'https://reactjs.org',
+    author: "Fish Man",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: 'https://redux.js.org',
+    author: "Andrew Clark, Dan Abramov",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
@@ -18,38 +37,29 @@ const App = () => {
     ''
   )
 
-  // const [storiesList, setStoriesList] = React.useState(stories);
+
+
+  const [stories, setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = (item) => {
+    console.log("handleRemoveStory: ", item.title)
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+  }
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   }
-
-  const stories = [
-    {
-      title: "React",
-      url: 'https://reactjs.org',
-      author: "Fish Man",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: 'https://redux.js.org',
-      author: "Andrew Clark, Dan Abramov",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
+  
   const searchedStories = stories.filter((story => {
     return story.title.toUpperCase().includes(searchTerm.toUpperCase());
   }));
 
   return (
     <div>
-      <h1 id="title">working title</h1>
+      <h1 id="title">stories</h1>
       <InputWithLabel
        id="search" 
        value={searchTerm}
@@ -57,7 +67,7 @@ const App = () => {
       >
       search:
       </InputWithLabel>
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
       <Button handleClick={() => console.log("Hi this is a button")}>
         Hi, this is a button.
       </Button>
@@ -96,33 +106,34 @@ const InputWithLabel = ({
   )
 } 
 
-const List = ({ list }) => {
-  return (
+const List = ({ list, onRemoveItem }) => (
   <ul>
-    {list.map(({ objectID, ...item }) => (
-      <Item key={item.objectID} {...item} />
+    {list.map((item) => (
+      <Item 
+        key={item.objectID} 
+        item={item} 
+        onRemoveItem={onRemoveItem} 
+      />
     ))}
   </ul>
+);
+
+const Item = ({ item, onRemoveItem }) => {
+
+  return (
+    <div>
+      <h3>{item.title}</h3>
+      <li>author: {item.author}</li>
+      <li>number of comments: {item.num_comments}</li>
+      <li>number of points: {item.points}</li>
+      <li>
+        want to learn more about {item.title}? click <a href={item.url}>here</a>.
+      </li>
+      <button type="button" onClick={onRemoveItem.bind(null, item)}>
+        delete
+      </button>
+    </div>
   )};
-
-const Item = ({ title, url, author, num_comments, points }) => {
-  const removeStoryFromState = () => {
-    console.log("deleting!")
-  }
-
-  return (<div>
-    <h3>{title}</h3>
-    <li>author: {author}</li>
-    <li>number of comments: {num_comments}</li>
-    <li>number of points: {points}</li>
-    <li>
-      want to learn more about {title}? click <a href={url}>here</a>.
-    </li>
-    <Button handleClick={removeStoryFromState}>
-      delete
-    </Button>
-  </div>
-)};
 
 const Button = ({ type = 'button', handleClick, children }) => (
   <button type={type} onClick={handleClick}>
