d4b6bb96b5455978f12785f8512abc8af4af4d15
atbeesley <alexander.beesley@gmail.com>
2023-04-18 16:07:11 +0100

    fetching stories from api + title change

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,6 @@
 import * as React from "react";
 
-const initialStories = [
-  {
-    title: "React",
-    url: "https://reactjs.org",
-    author: "Fish Man",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org",
-    author: "Andrew Clark, Dan Abramov",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(() => resolve({ data: { stories: initialStories } }), 5000)
-  );
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
@@ -81,11 +59,12 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits,
         });
         setIsLoading(false);
       })
@@ -111,7 +90,7 @@ const App = () => {
 
   return (
     <div>
-      <h1 id="title">stories</h1>
+      <h1 id="title">the react chronicles</h1>
       <InputWithLabel
         id="search"
         value={searchTerm}
