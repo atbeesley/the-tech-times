7d4fd5ec2d7fc68488731ba80bb2f84596e43449
atbeesley <alexander.beesley@gmail.com>
2023-04-18 09:13:55 +0100

    fetching stories async

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,9 @@ const initialStories = [
   },
 ];
 
+const getAsyncStories = () => 
+  new Promise((resolve) => setTimeout(() => resolve ({ data: { stories: initialStories } }), 2000));
+
 const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
@@ -34,7 +37,13 @@ const useStorageState = (key, initialState) => {
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState("search", "");
 
-  const [stories, setStories] = React.useState(initialStories);
+  const [stories, setStories] = React.useState([]);
+
+  React.useEffect(() => {
+    getAsyncStories().then(result => {
+      setStories(result.data.stories);
+    });
+  }, []);
 
   const handleRemoveStory = (item) => {
     const newStories = stories.filter(
